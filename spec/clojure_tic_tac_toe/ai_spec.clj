4061cd83c2f2047a0d1997b8007e5e94892d0336
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-05 15:54:04 -0600

    Updated function to generate board, updated specs

--- spec/clojure_tic_tac_toe/ai_spec.clj
+++ spec/clojure_tic_tac_toe/ai_spec.clj
@@ -5,13 +5,35 @@
 (describe "AI"
   (it "picks a random move given a 3x3 board"
     (should= 7
-      (pick-move ["X" "X" "X" "O" "X" "O" "O" "X" 8])))
+      (pick-move ["X" "X" "X"
+                  "O" "X" "O"
+                  "O" "X" 8])))
 
   (it "picks a random move given a 3x3 board"
     (should= 1
-      (pick-move ["X" "X" 2 "O" "X" "O" "O" "X" "O"])))
+      (pick-move ["X" "X" 2
+                  "O" "X" "O"
+                  "O" "X" "O"])))
 
-  (it "picks a random move given a 3x3 board"
-    (should= 7
-      (ai-move [1 2 3 "O" "X" "O" "O" "X" 8] 0 "O")))
+  (it "generates an end state board with a winner"
+    (should= ["O" 2 3
+              "O" "X" "O"
+              "O" "X" 9]
+      (generate-board   [1   2   3
+                        "O" "X" "O"
+                        "O" "X"  9] 0 "O")))
+
+  (it "reaches depth of 5 before generating an end state for that board"
+      (should= true
+       (generate-board [1   2   3
+                        4   5  "X"
+                        7   8   9] 0 "O")))
+
+  (it "generates an end state board with no winner"
+      (should= ["X" "O" "O"
+                "O" "X" "X"
+                "X" "X" "O"]
+        (generate-board ["X"  2  "O"
+                         "O" "X" "X"
+                         "X"  8  "O"] 0 "O")))
 )
--- src/clojure_tic_tac_toe/ai.clj
+++ src/clojure_tic_tac_toe/ai.clj
@@ -4,14 +4,41 @@
 (defn- switch-players [player-piece cells]
   (board/find-opponent-piece player-piece cells))
 
+(defn generate-board
+  ([cells depth player-piece]
+   (let [depth depth
+         open-space (- (nth (board/find-open-spaces cells) 0) 1)
+         depth depth
+         player-piece player-piece
+         cells cells
+         ]
+     (generate-board open-space depth player-piece cells)))
+
+  ([open-space depth player-piece cells]
+  (let [
+        open-space open-space
+        depth depth
+        player-piece player-piece
+        opponent-piece (switch-players player-piece cells)
+        updated-board (board/place-move open-space player-piece cells)
+        ]
+    (cond
+      (= depth 5) true
+      (board/game-over? player-piece opponent-piece updated-board)
+         updated-board
+      :else (recur (- (nth (board/find-open-spaces updated-board) 0) 1) (inc depth) (switch-players player-piece cells) updated-board)))))
+
 (defn ai-move [cells depth player-piece]
   (loop [index 0 open-cell (- (nth (board/find-open-spaces cells) index) 1) game-piece player-piece depth depth]
-    (let [updated-board (board/place-move open-cell game-piece cells)]
-      (println updated-board))
-      (println open-cell)
+    (let [updated-board (board/place-move open-cell game-piece cells)
+          other-piece (switch-players player-piece updated-board)
+          ]
+      (println player-piece)
+      (println updated-board)
+      (println open-cell))
     (cond
       (= depth 4) true
-      :else (recur (inc index) (- (nth (board/find-open-spaces cells) index) 1) (switch-players game-piece cells) (inc depth)))))
+      :else (recur (inc index) (- (nth (board/find-open-spaces cells) index) 1) (switch-players player-piece cells) (inc depth)))))
 
 (defn pick-move [cells]
   (- (rand-nth (board/find-open-spaces cells)) 1))
