12311c31ba112e8ec8b005d59a23e7f0c578d78e
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-08 20:59:03 -0500

    Added integration test for ai vs ai

--- spec/clojure_tic_tac_toe/game_spec.clj
+++ spec/clojure_tic_tac_toe/game_spec.clj
@@ -27,4 +27,29 @@
                 :ai-marker "X"
                 :board [1 2 3 4 5 6 7 8 9]}
         (get-options))))
+
+  (it "should get the ai move if current player-type is ai"
+      (should= 2
+        (get-move ["X" "X"  3
+                   "O"  5   6
+                    7   8   9] "ai" "O" "X")))
+
+  (it "should get the human move if current-piece is not ai"
+     (with-redefs [read-line (constantly "3")]
+      (should= 2
+        (get-move ["X" "X"  3
+                   "O"  5   6
+                    7   8   9] "human" "X" "O"))))
+
+  (it "should play ai vs ai game"
+    (let [board [1 2 3 4 5 6 7 8 9]
+          game-over-message "game over"]
+      (with-redefs [println (constantly "game over")
+                    prn (constantly true)]
+      (should= game-over-message
+        (start-game {:second-type "ai"
+                     :first-type "ai"
+                     :human-marker "X"
+                     :ai-marker "O"
+                     :board [1 2 3 4 5 6 7 8 9]})))))
 )
