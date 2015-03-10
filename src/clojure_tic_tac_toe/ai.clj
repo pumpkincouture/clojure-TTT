2e1f9a01d0a779f3f7822ce6016faad791406ad9
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-10 11:32:17 -0500

    Updated scores function, added get scores function to call update-each-space and get the scores of the remaining cells (flipping back and forth)

--- src/clojure_tic_tac_toe/ai.clj
+++ src/clojure_tic_tac_toe/ai.clj
@@ -1,6 +1,8 @@
 (ns clojure-tic-tac-toe.ai
   (:require [clojure-tic-tac-toe.board :as board]))
 
+(declare update-each-space)
+
 (def ^:const win 10)
 (def ^:const loss -10)
 (def ^:const tie 0)
@@ -9,6 +11,7 @@
   (board/find-opponent-piece current-player cells))
 
 (def max-player (atom ""))
+(def min-player (atom "X"))
 
 (defn set-max-player [player-piece]
   (reset! max-player player-piece))
@@ -16,13 +19,25 @@
 (defn score-board [cells current-player depth]
   (let [opponent (switch-players current-player cells)]
     (cond
-      (= (board/winner? current-player opponent cells) @max-player) (- win depth)
+      (= (board/winner? current-player opponent cells) current-player) (- win depth)
       (= (board/winner? current-player opponent cells) opponent) (- loss depth)
       :else tie)))
 
 (defn get-scores [cells current-player depth]
-  (board/game-over? current-player (switch-players current-player cells) cells) (score-board cells current-player depth)
-  (println "hello from get-scores, game isnt over"))
+  (board/game-over? current-player (switch-players current-player cells) cells)
+    (println (score-board cells current-player depth))
+  (let [scores-list (update-each-space cells current-player depth)]
+    (cond
+      (= current-player @max-player) (apply max scores-list)
+      :else (apply min scores-list))))
+
+(defn add-to-scores-list [space score]
+  [space score])
+
+(defn update-each-space [cells current-player depth]
+  (let [open-spaces (board/find-open-spaces cells)]
+    (for [space open-spaces]
+      (add-to-scores-list space (get-scores cells current-player depth)))))
 
 (defn place-scored-move [cells current-player space]
   (let [updated-board (board/place-move (dec space) current-player cells)
