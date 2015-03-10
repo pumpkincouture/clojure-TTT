ee7a187144ee97cacdebf11b53cbaa91581b27b6
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-10 15:51:12 -0500

    Most of ai tests passing, with the exception of placing second move on the board and blocking opponent in top right corner

--- spec/clojure_tic_tac_toe/ai_spec.clj
+++ spec/clojure_tic_tac_toe/ai_spec.clj
@@ -5,39 +5,39 @@
 (describe "AI"
   (it "generates a score of 10 for computer win"
       (should= 10
-        (score-board ["T"  2  "T"
-                      "T" "H" "H"
-                      "T"  8  "T"] (set-max-player "T") 0)))
+        (score-board ["O"  2  "X"
+                      "O" "X" "X"
+                      "O"  8  "X"] "O" 0)))
 
   (it "generates a score of -10 for opponent win"
       (should= -10
-        (score-board ["H"  2  "T"
-                      "H" "T" "H"
-                      "H"  8  "T"] (set-max-player "T") 0)))
+        (score-board ["X"  2  "O"
+                      "X" "T" "X"
+                      "X"  8  "O"] "O" 0)))
 
   (it "generates a score of -10 for computer loss"
       (should= -10
-        (score-board ["H"  2  "H"
-                      "T" "H" "H"
-                      "H"  8  "T"] (set-max-player "T") 0)))
+        (score-board ["X"  2  "X"
+                      "O" "X" "X"
+                      "X"  8  "O"] "O" 0)))
 
   (it "returns a score of -10 for computer loss"
       (should= -10
-        (score-board ["H"  2  "H"
-                      "T" "T" "H"
-                      "H"  8  "H"] (set-max-player "T") 0)))
+        (score-board ["X"  2  "X"
+                      "O" "O" "X"
+                      "X"  8  "X"] "O" 0)))
 
   (it "returns a score of -10 for opponent win"
       (should= -10
-        (score-board [ 1   2  "T"
-                      "H" "T" "H"
-                      "H" "H" "H"] (set-max-player "T") 0)))
+        (score-board [ 1   2  "O"
+                      "X" "O" "X"
+                      "X" "X" "X"] "O" 0)))
 
   (it "generates a score of 0 for tie board"
       (should= 0
-        (score-board ["T" "H" "T"
-                      "H" "T" "H"
-                      "H" "T" "H"] (set-max-player "T") 0)))
+        (score-board ["O" "X" "O"
+                      "X" "O" "X"
+                      "X" "O" "X"] "O" 0)))
 
   (it "returns X if O is the current player"
       (should= "X"
@@ -62,9 +62,45 @@
                   "O" "X" "X"
                   "X"  8   9] 0 (set-max-player "O"))))
 
-  (it "returns 7 as best move"
+  (it "returns 8 as best move"
       (should= 8
         (ai-move ["X" "O" "O"
                   "O" "O" "X"
                    7   8   9] 0 (set-max-player "O"))))
+
+  (it "returns 2 as best move"
+      (should= 2
+        (ai-move ["O"  2  "O"
+                   4  "X" "X"
+                   7   8   9] 0 (set-max-player "O"))))
+
+  (it "returns a block to opponent"
+      (should= 9
+        (ai-move ["X"  2   3
+                   4  "X"  6
+                  "O"  8   9] 0 "O")))
+
+  (it "blocks opponent in top right corner"
+      (should= 3
+        (ai-move ["X" "X"  3
+                   4   5   6
+                  "O"  8   9] 0 "O")))
+
+   (it "blocks opponent in the bottom left corner"
+      (should= 7
+        (ai-move [ 1  "O" "X"
+                   4  "X"  6
+                   7   8   9] 0 "O")))
+
+   (it "returns best move if computer makes second move"
+      (should= 5
+        (ai-move ["X"  2   3
+                   4   5   6
+                   7   8   9] 0 "O")))
+
+   (it "returns best move if computer makes first move"
+      (should= 5
+        (ai-move [ 1   2   3
+                   4   5   6
+                   7   8   9] 0 "O")))
 )
--- src/clojure_tic_tac_toe/ai.clj
+++ src/clojure_tic_tac_toe/ai.clj
@@ -10,7 +10,7 @@
 (defn switch-players [current-player cells]
   (board/find-opponent-piece current-player cells))
 
-(def max-player (atom ""))
+(def max-player (atom "O"))
 (def min-player (atom "X"))
 
 (defn set-max-player [player-piece]
@@ -20,11 +20,11 @@
   (let [opponent (switch-players current-player cells)]
     (cond
       (= (board/winner? current-player opponent cells) "O") (- win depth)
-      (= (board/winner? current-player opponent cells) opponent) (- loss depth)
+      (= (board/winner? current-player opponent cells) "X") (- loss depth)
       :else tie)))
 
 (defn get-scores [cells current-player depth]
-  (println current-player depth cells "current player, depth and cells from get-scores, ")
+  ;(println current-player depth cells "current player, depth and cells from get-scores, ")
    (if (board/game-over? current-player (switch-players current-player cells) cells)
       (score-board cells current-player depth)
       (let [scores-list (vec (update-each-space cells current-player depth))]
@@ -32,8 +32,8 @@
       (apply min scores-list)))))
 
 (defn update-each-space [cells current-player depth]
-  (println cells "cells from update each space")
-  (println (board/find-open-spaces cells) "remaining cells")
+  ;(println cells "cells from update each space")
+  ;(println (board/find-open-spaces cells) "remaining cells")
   (let [open-spaces (board/find-open-spaces cells)]
     (for [space open-spaces]
       (get-scores (board/place-move (dec space) (switch-players current-player cells) cells) (switch-players current-player cells) (inc depth)))))
@@ -44,7 +44,7 @@
 
 (defn get-spaces [cells current-player depth]
   (let [open-spaces (board/find-open-spaces cells)]
-    (println open-spaces "open spaces from get-spaces")
+    ;(println open-spaces "open spaces from get-spaces")
     (for [space open-spaces]
       (place-scored-move cells current-player space depth))))
 
@@ -53,9 +53,9 @@
 
 (defn get-move [cells depth current-player]
   (set-max-player max-player)
-  (println (get-spaces cells current-player depth) "this is from get-move")
+  ;(println (get-spaces cells current-player depth) "this is from get-move")
   (update-scores-list cells depth current-player))
 
 (defn ai-move [cells depth current-player]
-  (println (get-move cells depth current-player) "printed from ai move")
+  (println (get-move cells depth current-player) "scores from ai move")
   (first (first (sort-by second > (get-move cells depth current-player)))))
