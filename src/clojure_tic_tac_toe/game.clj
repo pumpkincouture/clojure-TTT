af133ac086539283201ec9c6a48754ae5f912270
Sylwia Olak <pumpkincouture@gmail.com>
2015-03-18 14:48:19 -0500

    Changed loop to how it was, without atoms

--- src/clojure_tic_tac_toe/game.clj
+++ src/clojure_tic_tac_toe/game.clj
@@ -9,14 +9,6 @@
 (def first-player (atom {}))
 (def second-player (atom {}))
 
-;(defmulti get-move (fn [current-player cells] (:player-type current-player)))
-;
-;(defmethod get-move "human" [current-player cells]
-;  (get-human-move cells current-player))
-;
-;(defmethod get-move "ai" [current-player cells]
-;  (ai/ai-move cells 0 current-player))
-
 (defn valid-input? [input]
   (cond
     (integer? (read-string input)) (dec (Integer/parseInt input))
@@ -33,38 +25,25 @@
          [input (get-human-move board player-piece)]
           input))))
 
-(defn get-move [current-player board]
-  (if (= (current-player :player-type) (second player-types))
-      (dec (ai/ai-move board 0 (current-player :player-piece)))
-      (get-human-move board (current-player :player-piece))))
+(defn get-move [board current-type current-mark next-mark]
+    (if (= current-type (second player-types))
+            (dec (ai/ai-move board 0 current-mark))
+                  (get-human-move board current-mark)))
 
 (defn game-loop [first-piece second-piece current-type next-type board]
-     (loop [first-piece  first-piece
-            second-piece  second-piece
-            current-type current-type
-            next-type next-type
-            board board]
-     (if (board/game-over? first-piece second-piece board)
-         (ui/print-result first-piece second-piece board)
-         (do
-           (let [move (get-move board current-type first-piece second-piece)]
-          (let [updated-board (board/place-move move first-piece board)]
-         (ui/print-choice first-piece move)
-         (ui/print-board updated-board)
-        (recur second-piece first-piece next-type current-type updated-board))))))))
-
-(defn game-loop [first-player second-player board]
-   (loop [first-player  first-player
-          second-player  second-player
-          board board]
-    (if (board/game-over? (first-player :player-piece) (second-player :player-piece) board)
-       (ui/print-result (first-player :player-piece) (second-player :player-piece) board)
-          (do
-            (let [move (get-move (first-player :player-type) board)]
-            (let [updated-board (board/place-move move (first-player :player-piece) board)]
-            (ui/print-choice ((board/current-player board first-player second-player) :player-piece) move)
-            (ui/print-board updated-board)
-            (recur second-player first-player updated-board)))))))
+ (loop [first-piece  first-piece
+        second-piece  second-piece
+        current-type current-type
+        next-type next-type
+        board board]
+  (if (board/game-over? first-piece second-piece board)
+      (ui/print-result first-piece second-piece board)
+      (do
+       (let [move (get-move board current-type first-piece second-piece)]
+       (let [updated-board (board/place-move move first-piece board)]
+      (ui/print-choice first-piece move)
+      (ui/print-board updated-board)
+    (recur second-piece first-piece next-type current-type updated-board)))))))
 
 (defn get-human-option []
   (first player-types))
@@ -93,4 +72,4 @@
   (swap! first-player assoc :player-type first-player-type :player-piece first-piece)
   (swap! second-player assoc :player-type second-player-type :player-piece second-piece)
   (ui/print-board board)
-  (game-loop @first-player @second-player board)))
+ (game-loop first-piece second-piece first-player-type second-player-type board)))
